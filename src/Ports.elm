f3825e95e165f95a0e6543001ccb4ca484ebdd54
Andrew Hyndman <ajhyndman@hotmail.com>
2016-05-29 20:35:52 +1000

    Document function of selection port

--- src/Ports.elm
+++ src/Ports.elm
@@ -1,7 +1,9 @@
-port module Ports exposing (..)
+port module Ports exposing (selection)
 
 
 import Model
 
 
+-- This port receives updates whenever $('document').on('selectionchange')
+-- fires.
 port selection : (Model.Selection -> message) -> Sub message
