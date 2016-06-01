bfee76d55a1990eeaeb32b12e371add6ec4f53b1
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-01 22:00:35 +1000

    Implement basic editor component

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import EditorPanel from './components/EditorPanel.jsx';
 
-const Hello = (props) => (
-  <div>{`Hello ${props.name}`}</div>
-);
-
-Hello.propTypes = {
-  name: React.PropTypes.string,
-};
 
 ReactDOM.render(
-  <Hello name="Violet!" />,
+  <EditorPanel />,
   document.getElementById('app')
 );
