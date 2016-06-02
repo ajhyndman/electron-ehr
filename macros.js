60e1898478152f2a49be501f3bd849760364d577
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-03 02:47:30 +1000

    Extract macros into a json object

---
+++ macros.js
@@ -0,0 +1,8 @@
+
+
+const macros = {
+  '.zisis': 'Specifically, I reviewed in detail with this patient, the Internation Spinal Intervention Society (ISIS) diagnostic and therapeutic paradigm for facet joint spondylosis. We reviewed the value of cervicallumbarmedial branch nerve block proven facet spondylosis for longer term palliation with RF neurolysis of the same. We reviewed procedural technique, potential risks, and reasonable expectations.',
+};
+
+
+export default macros;
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,14 @@
 import { EditorState, Modifier } from 'draft-js';
 
+import macros from 'macros.js';
+
 
 const reducer = function reducer(state, action) {
   switch (action.type) {
-  case 'edit':
+  case 'edit': {
     return state.set('editor', action.next);
-  case 'macro':
+  }
+  case 'macro': {
     const editorState = state.get('editor');
 
     // Don't trigger macros if more than one character is selected.
@@ -23,28 +26,35 @@ const reducer = function reducer(state, action) {
       .getText()
       .slice(0, focusOffset);
 
-    if (/.zisis$/.test(candidateText)) {
-      const expandedText = candidateText.replace(
-        /.zisis$/,
-        'Specifically, I reviewed in detail with this patient, the Internation Spinal Intervention Society (ISIS) diagnostic and therapeutic paradigm for facet joint spondylosis. We reviewed the value of cervicallumbarmedial branch nerve block proven facet spondylosis for longer term palliation with RF neurolysis of the same. We reviewed procedural technique, potential risks, and reasonable expectations.'
-      );
 
-      const nextContentState = Modifier.replaceText(
-        editorState.getCurrentContent(),
-        editorState.getSelection().merge({ anchorOffset: 0 }),
-        expandedText
-      );
+    for (const key of Object.keys(macros)) {
+      const macroRegex = new RegExp(`${key}$`);
 
-      const nextState = EditorState.push(editorState, nextContentState, 'insert-characters');
+      if (macroRegex.test(candidateText)) {
+        const expandedText = candidateText.replace(
+          macroRegex,
+          macros[key]
+        );
 
-      return state.set(
-        'editor',
-        nextState
-      );
+        const nextContentState = Modifier.replaceText(
+          editorState.getCurrentContent(),
+          editorState.getSelection().merge({ anchorOffset: 0 }),
+          expandedText
+        );
+
+        const nextState = EditorState.push(editorState, nextContentState, 'insert-characters');
+
+        return state.set(
+          'editor',
+          nextState
+        );
+      }
     }
     return state;
-  default:
+  }
+  default: {
     return state;
+  }
   }
 };
 
