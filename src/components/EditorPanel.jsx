821ba194d9f6ab4772469f070a36aa6b0aa1f8cb
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-03 01:58:56 +1000

    Implement simple macro expansion

--- src/components/EditorPanel.jsx
+++ src/components/EditorPanel.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import ReactRedux from 'react-redux';
+import { connect } from 'react-redux';
 import { Editor } from 'draft-js';
 
-import { edit } from './actions';
+import actions from '../actions';
+import keybindings from '../keybindings';
 
 
 const EditorPanel = (props) => (
   <Editor
     editorState={props.editorState}
+    keyBindingFn={keybindings}
     onChange={props.onChange}
+    onTab={props.onTab}
   />
 );
 
 EditorPanel.propTypes = {
   editorState: React.PropTypes.object.isRequired,
   onChange: React.PropTypes.func.isRequired,
+  onTab: React.PropTypes.func.isRequired,
 };
 
 
-export default ReactRedux.connect(
+export default connect(
   (state) => ({ editorState: state.get('editor') }),
-  (dispatch) => ({ onChange: (next) => dispatch(edit(next)) })
+  (dispatch) => ({
+    onChange: (next) => dispatch(actions.edit(next)),
+    onTab: function onTab(event) {
+      event.preventDefault();
+      dispatch(actions.macro());
+    },
+  })
 )(EditorPanel);
