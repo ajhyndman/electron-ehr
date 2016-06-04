c4e13597c3bb3d28f5ddc526ec1415485e2bdd4c
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-04 12:29:25 +1000

    Sort imports

--- src/components/EditorPanel.jsx
+++ src/components/EditorPanel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Editor } from 'draft-js';
+import { connect } from 'react-redux';
 
 import actions from '../actions';
 import keybindings from '../keybindings';
