7315fad008aecf25aa6a8eb2339fe3c2665360e9
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-05 16:12:38 +1000

    Add plus and minus icons

---
+++ src/components/icons/Remove.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Remove = (props) => (
+  <svg
+    fill="#000000"
+    style={{
+      display: 'block',
+      lineHeight: '1em',
+    }}
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M19 13H5v-2h14v2z" />
+    <path d="M0 0h24v24H0z" fill="none" />
+  </svg>
+);
+
+
+export default Remove;
