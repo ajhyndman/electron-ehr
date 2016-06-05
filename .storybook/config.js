5cfd81d2bd11c84459f6f98f64a21fde648d7a61
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-05 13:57:59 +1000

    Remove placeholder story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  require('../src/stories/button');
   require('../src/stories/EditorPanel');
   // require as many stories as you need.
 }
