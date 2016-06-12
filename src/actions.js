1adbd1a7c5e295bf09c952cefe2260d3a9eca5ab
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-12 22:48:49 +1000

    Add new tab actions

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,8 @@
 
 
 const actions = {
+  ACTIVATETAB: (key) => ({ type: 'ACTIVATETAB', key }),
+  CLOSETAB: (key) => ({ type: 'CLOSETAB', key }),
   EDIT: (next) => ({ type: 'EDIT', next }),
   MACRO: () => ({ type: 'MACRO' }),
   TOGGLE: (key) => ({ type: 'TOGGLE', key }),
