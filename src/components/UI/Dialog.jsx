96308e2006eba731e2dbbca9c8f2fa1c5797a155
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-19 21:46:56 +1000

    Add backdrop to <Dialog>

--- src/components/UI/Dialog.jsx
+++ src/components/UI/Dialog.jsx
@@ -2,27 +2,40 @@ import React from 'react';
 
 
 const Dialog = (props) => (
-  <dialog
+  // backdrop
+  <div
     style={{
-      border: 0,
-      boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.2)',
-      fontFamily: [
-        '"Segoe UI"',
-        'Frutiger',
-        '"Frutiger Linotype"',
-        '"Dejavu Sans"',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-      ].join(', '),
+      background: 'rgba(0, 0, 0, 0.3)',
+      bottom: 0,
+      display: (props.open ? 'block' : 'none'),
+      left: 0,
       position: 'fixed',
+      right: 0,
       top: 0,
-      width: 400,
     }}
-    open={props.open}
   >
-    {props.children}
-  </dialog>
+    <dialog
+      style={{
+        border: 0,
+        boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.2)',
+        fontFamily: [
+          '"Segoe UI"',
+          'Frutiger',
+          '"Frutiger Linotype"',
+          '"Dejavu Sans"',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+        ].join(', '),
+        position: 'fixed',
+        top: 0,
+        width: 400,
+      }}
+      open={props.open}
+    >
+      {props.children}
+    </dialog>
+  </div>
 );
 
 Dialog.propTypes = {
