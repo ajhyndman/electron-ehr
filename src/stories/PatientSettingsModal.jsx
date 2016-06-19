d26535fef044f70ed239e73c59cef9fd44ba1f71
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-19 18:25:06 +1000

    Build Patient Settings Dialog

---
+++ src/stories/PatientSettingsModal.jsx
@@ -0,0 +1,23 @@
+import 'normalize.css';
+import Immutable from 'immutable';
+import React from 'react';
+import { action, storiesOf } from '@kadira/storybook';
+
+import PatientSettingsModal from 'components/PatientSettingsModal';
+
+
+const patient = Immutable.Map({
+  firstName: 'Andrew',
+  lastName: 'Hyndman',
+  dob: '1988-10-06',
+  gender: 'm',
+  address: '131-145 Glebe Point Rd, NSW, 2037',
+});
+
+storiesOf('New Patient Modal', module)
+  .add('open', () => (
+    <PatientSettingsModal
+      patient={patient}
+      onChange={action('UPDATE PATIENT')}
+    />
+  ));
