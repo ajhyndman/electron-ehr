4e0bed0549b002fb6e4b0c0d06e5f056c0af8aee
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-23 01:26:30 +1000

    Parse brackets '[' in templates

--- src/draftUtils/createFromTemplate.js
+++ src/draftUtils/createFromTemplate.js
@@ -1,14 +1,50 @@
-import { ContentState, EditorState } from 'draft-js';
+import UUID from 'uuid-js';
+import { EditorState, convertFromRaw } from 'draft-js';
 
 import compositeDecorator from 'decorators/all';
 
 
 function createFromTemplate(template = '') {
-  const templateContent = ContentState.createFromText(template);
+  const entityMap = {};
+  const entityRanges = [];
 
-  // TODO: Parse Toggles
+  let offsetCorrection = 0;
+
+  // Parse Toggles
+  const cleanedText = template.replace(
+    /(\[\s*)(.*?)(\s*\])/g,
+    function (match, p1, p2, p3, offset) {
+      offsetCorrection += p1.length;
+      const key = UUID.create();
+      entityMap[key] = {
+        type: 'TOGGLE',
+        mutability: 'IMMUTABLE',
+      };
+      entityRanges.push({
+        key,
+        length: p2.length,
+        offset: offset + p1.length - offsetCorrection,
+      });
+      offsetCorrection += p3.length;
+      return p2;
+    }
+  );
+
   // TODO: Parse Sections
+
   // TODO: Parse patient-specific text
+
+  // Construct contentState
+  const templateContent = convertFromRaw({
+    blocks: [
+      {
+        text: cleanedText,
+        type: 'unstyled',
+        entityRanges,
+      },
+    ],
+    entityMap,
+  });
 
   return EditorState.createWithContent(
     templateContent,
