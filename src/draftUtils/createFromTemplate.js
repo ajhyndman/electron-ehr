3beaf1faaaa9b908e8682ffbba1071e0ee62f314
Andrew Hyndman <ajhyndman@hotmail.com>
2016-06-22 23:02:46 +1000

    NEWTAB action should build the content from template if received

--- src/draftUtils/createFromTemplate.js
+++ src/draftUtils/createFromTemplate.js
@@ -1,10 +1,19 @@
-import { EditorState } from 'draft-js';
+import { ContentState, EditorState } from 'draft-js';
 
 import compositeDecorator from 'decorators/all';
 
 
-function createFromTemplate(template) {
-  return EditorState.createEmpty(compositeDecorator);
+function createFromTemplate(template = '') {
+  const templateContent = ContentState.createFromText(template);
+
+  // TODO: Parse Toggles
+  // TODO: Parse Sections
+  // TODO: Parse patient-specific text
+
+  return EditorState.createWithContent(
+    templateContent,
+    compositeDecorator
+  );
 }
 
 
