a5cd2754812a4c19c1e35da846b46857da252a4f
Andrew Hyndman <ajhyndman@hotmail.com>
2016-09-10 19:02:44 +1000

    Delete outdated store data shape comment

--- src/store.js
+++ src/store.js
@@ -11,40 +11,7 @@ import rootSaga from 'sagas';
 
 /**
  * Redux Store
- *
- * The below sample code snippet represents a snapshot of what the Redux Store
- * might look like during application execution.
  */
-
-// import { EditorState } from 'draft-js';
-// import compositeDecorator from 'decorators/all';
-
-// const sampleStore = Map({
-//   activeTab: 0,
-//   editors: List([
-//     Map({
-//       patient: Map({
-//         firstName: '하라',
-//         lastName: '주',
-//         dob: '1991-11-22',
-//         gender: 'f',
-//         address: '',
-//       }),
-//       state: EditorState.createEmpty(compositeDecorator),
-//     }),
-//     Map({
-//       patient: Map({
-//         firstName: 'Andrew',
-//         lastName: 'Hyndman',
-//         dob: '1988-10-06',
-//         gender: 'm',
-//         address: '131-145 Glebe Point Rd, Glebe, NSW 2037, Australia',
-//       }),
-//       state: EditorState.createEmpty(compositeDecorator),
-//     }),
-//   ]),
-//   patientSettingsOpen: false,
-// });
 
 export type Patient = {
   address?: string;
